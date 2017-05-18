99e99a5fa377a34c2cb7b833d2bb81d502055e33
bartdegraeuwe <bart.degraeuwe@jrc.europa.eu>
2017-05-18 17:17:17 +0200

    first screening added for header and number of columns. Reorganizing the detection of different formats

--- read_TrackNEDCHot_Bart_20170517.R
+++ read_TrackNEDCHot_Bart_20170517.R
@@ -16,8 +16,9 @@ Track.hotNEDC.results.2 <- data.frame()
 Track.hotNEDC.results.3 <- data.frame()
 Track.hotNEDC.results.4 <- data.frame()
 
-overview.ukdata <- data.frame(car.name = c(), header1 = as.character(), num.cols = as.integer())
+overview.TrackHotNEDC <- data.frame(car.name = c(), header1 = as.character(), num.cols = as.integer())
 
+# loop over all test to have an overview of the existing formats
 for (path in paths) {
   xlsx.files <- list.files(path)
   for (file in xlsx.files) {
@@ -29,22 +30,30 @@ for (path in paths) {
     sheet.number <- match("T-Hot NEDC", sheets)
     if (is.na(sheet.number)) {
       print(paste('Sheet', "T-Hot NEDC", "not found in", file))
-      overview.ukdata <- rbind(overview.ukdata,
+      overview.TrackHotNEDC <- rbind(overview.TrackHotNEDC,
                                data.frame(car.name = car.name, header1 = NA, num.cols = NA))
     } else {
-      test.data <- read_excel(paste0(path, file), sheet = sheet.number)
+      test.data <- data.frame()
+      try(test.data <- read_excel(paste0(path, file), sheet = sheet.number), silent = TRUE)
       header.names <- names(test.data)
-      overview.ukdata <- rbind(overview.ukdata,
+      overview.TrackHotNEDC <- rbind(overview.TrackHotNEDC,
                                data.frame(car.name = car.name, 
                                           header1 = header.names[1], 
                                           num.cols = length(header.names)))
     }      
   }
 }
+overview.TrackHotNEDC.sorted <- overview.TrackHotNEDC[order(overview.TrackHotNEDC$header, overview.TrackHotNEDC$num.cols),]
+write.table(overview.TrackHotNEDC, 'overview_TrackHotNEDC.txt', row.names = FALSE, quote = FALSE)
 
+path <- paths[2]
+file <- "euro-6-ford-focus-1499.xlsx"
 
-path <- paths[1]
-file <- "euro-5-ford-mondeo-2000.xlsx"
+# loop over all test and put the results in a data frame according to the format.
+# The following formats exist:
+# 1) format 1: header[1] = Absolute_time and 138 columns (one test has the other format pasted to its right, 174 cols)
+# 2) format 2: header[1] = 'Test SequenceNumber' and 36 (3), 37 (1) or 52 (1) columns
+# 3) format 3: 'TestSequenceNumber'       52
 
 for (path in paths) {
   xlsx.files <- list.files(path)
@@ -52,27 +61,61 @@ for (path in paths) {
   for (file in xlsx.files) {
     print(paste('processing', file))
     car.name <- sub(".xlsx", "", file)
-    overview.ukdata <- rbind(overview.ukdata, data.frame(car.name = car.name, TrackHotNEDC = -1))
+    overview.TrackHotNEDC <- rbind(overview.TrackHotNEDC, data.frame(car.name = car.name, TrackHotNEDC = -1))
     
     # get the sheet number of the 'Track Hot NEDC' test
     sheets <- excel_sheets(paste0(path, file))
     # get the sheet number for the hot track test
     sheet.number <- match("T-Hot NEDC", sheets)
+    test.data <- data.frame()
+    try(test.data <- read_excel(paste0(path, file), sheet = sheet.number))
     
-    if (is.na(sheet.number)) {
-      
+    if (NROW(test.data) == 0) {
+      # no sheet or an empty sheet
       print(paste('Sheet', "T-Hot NEDC", "not found in", file))
-      overview.ukdata$TrackHotNEDC[overview.ukdata$car.name == car.name] <- NA
       
     } else {
+      # add a column with the car name and test type
+      test.data <- data.frame(car.name = car.name, test.type = 'T-Hot NEDC', test.data)
       
-      test.data <- read_excel(paste0(path, file), sheet = sheet.number)
+      # data are available, but in which format???
       # by retrieving the list of header names it's possible to check the kind of data
       header.names <- names(test.data)
 
-      # There are 2 types of files:
-      # 1) starting with 'TestSequenceNumber'
-      if (header.names[1] == 'TestSequenceNumber') {
+      if (header.names[1] == 'Absolute_time') {
+        # 2 possibilities: 138 columns or 174 columns (=138 + the other format)
+        # the plot is the same for both number of columns
+        jpeg(paste0('plots/', 'GPS_speed_track_', car.name, '.jpg'), width = 2*480, height = 480)
+        par(mfrow=c(1,2))
+        plot(test.data$GPS_VehicleSpeed, main = car.name, type = 'l') # on road NEDC
+        plot(test.data$GPS_Longitude, test.data$GPS_Latitude, type = 'l') # you can see the test track shape
+        dev.off()
+        
+        num.cols <- NCOL(test.data)
+        if (num.cols == 2+138) {
+          Track.hotNEDC.results.1 <- rbind(Track.hotNEDC.results.1, test.data)
+        } else if (num.cols == 2+174) {
+          # there's only 1 case, seems they compared the to PEMS during the same trip
+          # select only the first 2+138 columns
+          Track.hotNEDC.results.1 <- rbind(Track.hotNEDC.results.1, test.data[,1:140])
+        } else {
+          print('Problem: other number of columns for format Absolute_time')
+        }
+        
+        # # add data to dataframe (tibble)
+        # test.data <- data.frame(car.name = car.name, test.type = 'T-Hot NEDC', test.data)
+        # if (NCOL(test.data) == 140) {
+        #   Track.hotNEDC.results.2 <- rbind(Track.hotNEDC.results.2, test.data)
+        #   overview.TrackHotNEDC$TrackHotNEDC[overview.TrackHotNEDC$car.name == car.name] <- 2
+        # } else if (NCOL(test.data) == 176) {
+        #   Track.hotNEDC.results.3 <- rbind(Track.hotNEDC.results.3, test.data)
+        #   overview.TrackHotNEDC$TrackHotNEDC[overview.TrackHotNEDC$car.name == car.name] <- 3
+        # } else {
+        #   overview.TrackHotNEDC$TrackHotNEDC[overview.TrackHotNEDC$car.name == car.name] <- NCOL(test.data)
+        #   print(paste(car.name, 'has', NCOL(test.data), 'columns'))
+        # }
+        
+      } else if (header.names[1] == 'Test SequenceNumber') {
         
         # some plots to check the data
         jpeg(paste0('plots/', 'GPS_speed_track_', car.name, '.jpg'), width = 2*480, height = 480)
@@ -85,7 +128,7 @@ for (path in paths) {
         # add a column with the car name and test type
         test.data <- data.frame(car.name = car.name, test.type = 'T-Hot NEDC', test.data)
         Track.hotNEDC.results.1 <- rbind(Track.hotNEDC.results.1, test.data)
-        overview.ukdata$TrackHotNEDC[overview.ukdata$car.name == car.name] <- 1
+        overview.TrackHotNEDC$TrackHotNEDC[overview.TrackHotNEDC$car.name == car.name] <- 1
         
       } # stop reading format 1
       
@@ -102,39 +145,19 @@ for (path in paths) {
         # add a column with the car name and test type
         test.data <- data.frame(car.name = car.name, test.type = 'T-Hot NEDC', test.data)
         Track.hotNEDC.results.4 <- rbind(Track.hotNEDC.results.4, test.data)
-        overview.ukdata$TrackHotNEDC[overview.ukdata$car.name == car.name] <- 4
+        overview.TrackHotNEDC$TrackHotNEDC[overview.TrackHotNEDC$car.name == car.name] <- 4
         
       }
       
-      else if (header.names[1] == 'Absolute_time') {
-        jpeg(paste0('plots/', 'GPS_speed_track_', car.name, '.jpg'), width = 2*480, height = 480)
-        par(mfrow=c(1,2))
-        plot(test.data$GPS_VehicleSpeed, main = car.name, type = 'l') # on road NEDC
-        plot(test.data$GPS_Longitude, test.data$GPS_Latitude, type = 'l') # you can see the test track shape
-        dev.off()
-
-        # add data to dataframe (tibble)
-        test.data <- data.frame(car.name = car.name, test.type = 'T-Hot NEDC', test.data)
-        if (NCOL(test.data) == 140) {
-          Track.hotNEDC.results.2 <- rbind(Track.hotNEDC.results.2, test.data)
-          overview.ukdata$TrackHotNEDC[overview.ukdata$car.name == car.name] <- 2
-        } else if (NCOL(test.data) == 176) {
-          Track.hotNEDC.results.3 <- rbind(Track.hotNEDC.results.3, test.data)
-          overview.ukdata$TrackHotNEDC[overview.ukdata$car.name == car.name] <- 3
-        } else {
-          overview.ukdata$TrackHotNEDC[overview.ukdata$car.name == car.name] <- NCOL(test.data)
-          print(paste(car.name, 'has', NCOL(test.data), 'columns'))
-        }
-        
-      } # stop reading format 2
+      else 
       else {
         print('unknown header')
       }
-    }
+    } # if there are data
   } # loop over files
 } # loop over folders
 
-print(overview.ukdata)
+print(overview.TrackHotNEDC)
 
 length(unique(Track.hotNEDC.results.1$car.name))
 length(unique(Track.hotNEDC.results.2$car.name))
